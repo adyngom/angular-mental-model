1f8e8c1b4bb01d3b3ce3c32f1e7fc9dd16871819
Ady Ngom <ady.touba.ngom@gmail.com>
2021-11-11 12:31:13 -0500

    slight adjustment to theme

--- .storybook/fitmailTheme.js
+++ .storybook/fitmailTheme.js
@@ -2,7 +2,7 @@ import { create } from '@storybook/theming';
 
 export default create({
     base: 'light',
-    panelPosition: 'right',
+    panelPosition: 'bottom',
     brandTitle: 'Fitmail',
     brandImage: 'logo-text@4x.png',
     colorSecondary: '#b289ff',
